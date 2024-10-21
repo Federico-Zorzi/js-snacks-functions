eceab38a45da9b0c823bf28f373cad2e72982134
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-10-21 15:33:41 +0200

    ex 1 & 3 complited

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,18 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
+const vowels = ["a", "e", "i", "o", "u"];
 
 // Dichiara la funzione qui.
-
+const vowelsNumber = (word, vowelsArray) => {
+  let vowelsNum = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (vowelsArray.includes(word[i])) vowelsNum++;
+  }
+  console.log("il numero di vocali e': " + vowelsNum);
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+vowelsNumber(word, vowels);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
