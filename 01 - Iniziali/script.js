fad4ac6f485d3479dac379a080cf9e71d4700673
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-10-21 14:33:55 +0200

    first commit

---
+++ 01 - Iniziali/script.js
@@ -0,0 +1,13 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca un nuovo array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+const firstLetter = [];
+
+// ! PRIMA SOLUZIONE
+// Dichiara la funzione qui.
+names.forEach((currentValue) => firstLetter.push(currentValue[0]));
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(firstLetter);
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
---
+++ README.md
@@ -0,0 +1,9 @@
+# ex-functions-snack
+
+Ciao ragazzi, esercizio di oggi: Snack Funzioni
+repo: js-snacks-functions
+Consegna
+Risolvi gli snack che troverai in allegato definendo e invocando delle funzioni.
+Risolvere gli snack usando sia le funzioni tradizionali che le arrow functions.
+Bonus
+Risolvere lo snack nella cartellina 05 - bonusBuon divertimento
